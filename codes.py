3a45563c35477e8428ada038cb8f5e1f97738a05
imethw <165758092+imethw@users.noreply.github.com>
2024-07-11 00:21:24 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -41,7 +41,6 @@ sales_data = pd.read_excel("cleaned_dataset.xlsx", engine='openpyxl')
 # Creating charts to represent in the dashboard
 charts_info = [
     {"type": "box", "x": "Sub-Category", "y": "Quantity", "title": "Box Plot", "color": "green"},
-    {"type": "bar", "x": "Ship Mode", "y": "Shipping Cost", "title": "Bar Chart", "color": "#eba434"},
     {"type": "pie", "names": "Order Priority", "title": "Donut Chart", "hole": 0.5},
     {"type": "histogram", "x": "Region", "title": "Histogram"},  # Removed color parameter
     {"type": "scatter", "x": "Market", "y": "Profit", "title": "Scatter Plot"},
