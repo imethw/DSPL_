46d4297852cc4ff0ad9672339f55bc8428665e5b
imethw <165758092+imethw@users.noreply.github.com>
2024-07-11 01:05:23 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -80,6 +80,8 @@ nfo["type"])(df_top_10_countries, x=info.get("x", None), y=info.get("y", None),
             fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), title=info.get("title", None), color_discrete_sequence=["yellow"])
    elif info["type"] == "box":
             fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_discrete_sequence=["purple"])
+elif info["type"] == "scatter":
+            fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_discrete_sequence=["red"])
 
 
 
